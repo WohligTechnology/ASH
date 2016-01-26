cb8d14db33a01e1eeddedcfdadcf2ca4775ad66f
jayvisariya93 <jayvisariya93@gmail.com>
2016-01-26 11:34:45 +0530

    Removed slider, Launcher name and icon changed

--- app/src/main/java/com/wohlig/jaipurpinkpanthers/fragments/MerchandiseFragment.java
+++ app/src/main/java/com/wohlig/jaipurpinkpanthers/fragments/MerchandiseFragment.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.daimajia.slider.library.SliderLayout;
-import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.wohlig.jaipurpinkpanthers.R;
 import com.wohlig.jaipurpinkpanthers.util.CustomFonts;
 
@@ -28,22 +26,6 @@ public class MerchandiseFragment extends Fragment {
     }
 
     public void initilizeViews(){
-        SliderLayout sliderShow = (SliderLayout) view.findViewById(R.id.slider);
-
-        TextSliderView textSliderView = new TextSliderView(getActivity());
-        textSliderView
-                //.description("Game of Thrones")
-                .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
-
-        TextSliderView textSliderView1 = new TextSliderView(getActivity());
-        textSliderView1
-                //.description("Game of Thrones123")
-                .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
-
-        sliderShow.addSlider(textSliderView);
-        sliderShow.addSlider(textSliderView1);
-
-        //sliderShow.stopAutoCycle();
 
         tvBuyNow = (TextView) view.findViewById(R.id.tvBuyNow);
         tvBuyNow.setTypeface(CustomFonts.getRegularFont(getActivity()));
